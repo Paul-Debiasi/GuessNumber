b891e449361be7df1ec4a4fdf8149a52a2ced74d
Paul Debiasi <debiasi.paul@gmail.com>
2021-11-23 09:12:57 +0100

    finishing project

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
-// Bulls and Cows
-// Get library for user input
-// we need to keep the next line, so we can prompt the user for input
-// const prompt = require("prompt-sync")({ sigint: true });
-
-// // Test that prompt is working
-// let name = prompt("What is your name? ");
-// console.log(`User's input is: ${name}`);
-
-// Feel free to edit / remove the line above, this is just to test the package
-//  Although we may want to use the user's name for something
 let colorOne = Math.floor(Math.random() * 255) + 1;
 let colorTwo = Math.floor(Math.random() * 255) + 1;
 let colorT = Math.floor(Math.random() * 255) + 1;
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-secretNumber = Math.trunc(randomNumber(1000, 9999)) + "";
+let secretNumber = Math.trunc(randomNumber(1000, 9999)) + "";
 console.log("secret", secretNumber);
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  score = 20;
+
+  secretNumber = Math.trunc(randomNumber(1000, 9999)) + "";
+  console.log(`Secret inside addEvent ${secretNumber}`);
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  // document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = `#222`;
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".cows").textContent = "....";
+  rightMean.textContent = "";
+  document.querySelector(".highscore").textContent = 0;
+};
+
+let winner = document.querySelector(".winner");
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const hardMean = [
+  "Oh man you s...🤮",
+  "You should give up",
+  "Not even in thousand of attempts",
+  "No hope..",
+  "Good bye, I'm out...👋",
+  "What a ...💩",
+];
+let randomMean = Math.floor(Math.random() * hardMean.length);
+console.log(`${hardMean[randomMean]}`);
+
+let html = `${hardMean[randomMean]}`;
+const rightMean = document.querySelector(".mean-message");
 console.log(colorOne);
 
 let score = 20;
+let highScore = 0;
 let input = document.querySelector(".player");
 let playerTitle = document.querySelector(".title");
 input.onchange = handleChange;
@@ -43,28 +90,35 @@ const toggle = document
       level = false;
       easy.textContent = "Hard";
     }
-
+    rightMean.textContent = "";
     console.log(`${easy.textContent}`);
     console.log(`Level :${level}`);
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".bull").textContent = "...";
+    document.querySelector(".cows").textContent = "....";
   });
 // lo;
 // document.querySelector(".player").addEventListener("onchange", function () {});
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
-  // secretNumber;
-  console.log(`Secret inside addEvent ${typeof secretNumber}`);
+
+  secretNumber = Math.trunc(randomNumber(1000, 9999)) + "";
+  console.log(`Secret inside addEvent ${secretNumber}`);
 
   document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = score;
+  // document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = `#222`;
   document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".cows").textContent = "....";
+  rightMean.textContent = "";
+
+  console.log(`${level}`);
 });
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = String(document.querySelector(".guess").value);
-  console.log(`Level ${playerTitle.textContent}`);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number!";
@@ -79,19 +133,33 @@ document.querySelector(".check").addEventListener("click", function () {
     ).style.backgroundColor = `rgb(${colorOne},${colorTwo},${colorT})`;
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+
+    openModal();
+
+    winner.textContent = `${
+      playerName ? playerName : "Player1"
+    } is the winner, with a score of ${score}!!! 🎆👏🎈🔥`;
     if (score > highScore) {
       highScore = score;
+      console.log(`My current high Score ${score}`);
+
       document.querySelector(".highscore").textContent = highScore;
     }
   } else if (guess !== secretNumber) {
-    console.log(`Secret inside click event ${secretNumber}`);
     let count = 0;
     let bullEasy = "";
     let cowCount = 0;
+    let cowEasy = "";
     for (let i = 0; i < guess.length; i++) {
-      console.log(`${guess[i] === secretNumber[i]}`);
-
-      if (guess[i] === secretNumber[i]) {
+      console.log(` My level: ${level}`);
+      if (guess[i] === secretNumber[i] && level === false) {
+        count++;
+        document.querySelector(".bull").textContent = `${
+          count > 1 ? `You got ${count} bulls!` : `You got ${count} bull!`
+        }`;
+        document.querySelector(".guess").value = "";
+        console.log(`${playerName}`);
+      } else if (guess[i] === secretNumber[i]) {
         count++;
         bullEasy += secretNumber[i] + " - ".slice(-1);
         document.querySelector(".bull").textContent =
@@ -105,12 +173,21 @@ document.querySelector(".check").addEventListener("click", function () {
                 bullEasy ? bullEasy : null
               }`;
         document.querySelector(".guess").value = "";
+      } else if (secretNumber.split("").includes(guess[i]) && level === false) {
+        cowCount++;
+        document.querySelector(".cows").textContent = `${
+          cowCount > 1 ? `You got ${cowCount} cows` : `You got ${cowCount} cow!`
+        }`;
+        document.querySelector(".guess").value = "";
       } else if (secretNumber.split("").includes(guess[i])) {
         cowCount++;
+        cowEasy += `${guess[i]} `;
         console.log("Cow Count", cowCount);
-        document.querySelector(
-          ".cows"
-        ).textContent = `You got ${cowCount} cow!`;
+        document.querySelector(".cows").textContent = ` ${
+          cowCount > 1
+            ? `You got ${cowCount} cows! They are ${cowEasy}`
+            : `You got ${cowCount} cow! It is ${cowEasy}`
+        }`;
         document.querySelector(".guess").value = "";
       }
     }
@@ -119,6 +196,10 @@ document.querySelector(".check").addEventListener("click", function () {
         guess > secretNumber ? "Too high! Try again!" : "Too low! Try again!";
       score--;
       document.querySelector(".score").textContent = score;
+
+      level === false
+        ? (rightMean.textContent = html)
+        : (rightMean.textContent = "");
     } else {
       document.querySelector(".message").textContent = "You loose!";
       document.querySelector(".score").textContent = 0;
